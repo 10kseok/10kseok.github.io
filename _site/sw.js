ac5006b5cf46ae23ade0a2345c4b5e4e8e61a2d5
koesnam <76582376+10kseok@users.noreply.github.com>
2022-11-30 02:20:32 +0900

    rewrite url and project_name

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/10kseok.github.io/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221130.015102';
+const cacheName = 'chirpy-20221130.021847';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
