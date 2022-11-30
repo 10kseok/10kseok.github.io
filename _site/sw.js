c43abafa43c92287c471185fe6252d23a207ac47
koesnam <76582376+10kseok@users.noreply.github.com>
2022-11-30 11:36:09 +0900

    adjust pages-deploy.yml -> hook

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/10kseok.github.io/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221130.105425';
+const cacheName = 'chirpy-20221130.113525';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
