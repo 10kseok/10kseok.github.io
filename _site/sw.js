55c6fc5061590311a2c0dd547cad0936c9e4492d
koesnam <76582376+10kseok@users.noreply.github.com>
2022-11-30 10:51:08 +0900

    change ruby-version 2.7.7 -> 2.7

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/10kseok.github.io/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221130.023217';
+const cacheName = 'chirpy-20221130.104917';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
